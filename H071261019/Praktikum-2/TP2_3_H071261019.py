0ba0aad3b45baaae643be32cee2ce8849b2ce459
AkramZiyad17 <akramziyad078@gmail.com>
2026-09-25 15:21:49 +0800

    Revert "H071261097"

--- H071261019/Praktikum-2/TP2_3_H071261019.py
+++
@@ -1,9 +0,0 @@
-
-nilai = int(input("Masukkan nilai tes : "))
-pengalaman = int(input("Masukkan pengalaman kerja (tahun) : "))
-if nilai >= 80:
-    print("Lolos ke Tahap Wawancara")
-elif 65 <= nilai <= 80 and pengalaman >= 2:
-    print("Lolos Bersyarat")
-else:
-    print("Tidak Lolos")
--- H071261023/Praktikum-2/TP2_3_H071261023.py
+++
@@ -1,9 +0,0 @@
-nilai_tes = int(input("Masukkan nilai tes: "))
-pengalaman_kerja = int(input("Masukkan pengalaman kerja (tahun): "))
-
-if nilai_tes >=80:
-    print ("Lolos ke Tahap Wawancara")
-elif (nilai_tes <80 and nilai_tes >=65) and pengalaman_kerja >= 2:
-    print("Lolos Bersyarat")
-else:
-    print("Tidak Lolos")
